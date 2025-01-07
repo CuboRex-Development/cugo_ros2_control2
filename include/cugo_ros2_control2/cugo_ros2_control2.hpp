2ba3b7699afffffd445e9671f58b589275d90f68
abudori3939 <yutanakamura01@gmail.com>
2025-01-07 15:13:31 +0900

    追加：cppファイルを追加し、colcon buildが通るように整備

---
+++ include/cugo_ros2_control2/cugo_ros2_control2.hpp
@@ -0,0 +1,12 @@
+#ifndef CUGO_ROS2_CONTROL2_HPP
+#define CUGO_ROS2_CONTROL2_HPP
+
+#include <rclcpp/rclcpp.hpp>
+
+class CugoRos2Control2{
+    public:
+        CugoRos2Control2();
+        //void set_params();
+};
+
+#endif  // CUGO_ROS2_CONTROL2_HPP_
--- src/cugo_ros2_control2.cpp
+++ src/cugo_ros2_control2.cpp
@@ -1,10 +1,10 @@
-#include <cstdio>
+#include "cugo_ros2_control2/cugo_ros2_control2.hpp"
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
-
-  printf("hello world cugo_ros2_control2 package\n");
-  return 0;
+int main(int argc, char *argv[]) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("cugo_ros2_control2");
+    RCLCPP_INFO(node->get_logger(), "Cugo ROS 2 Control Node has started.");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
 }

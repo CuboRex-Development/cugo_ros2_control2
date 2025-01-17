2733a335ef410adecdd251225772306c51e799d0
abudori3939 <yutanakamura01@gmail.com>
2025-01-17 17:33:24 +0900

    追加：すべてのソースコードにコピーライトを追加

--- include/cugo_ros2_control2/serial.hpp
+++ include/cugo_ros2_control2/serial.hpp
@@ -1,7 +1,27 @@
+/*
+   Copyright [2025] [CuboRex Co.,Ltd.]
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 #ifndef CUGO_ROS2_CONTROL2_SERIAL_HPP
 #define CUGO_ROS2_CONTROL2_SERIAL_HPP
 
 #include <string>
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+#include <iostream>
+#include <algorithm>
+#include "cugo_ros2_control2/COBS.h"
 
 namespace cugo_ros2_control2
 {
